121e20c7ecb285172c13a1c97962abcdca8fa54a
Vadim <99600085+VadimFor@users.noreply.github.com>
2026-03-20 18:09:42 +0100

    vercel fix

--- custom-elements.d.ts
+++ custom-elements.d.ts
@@ -1,7 +1,11 @@
-declare namespace JSX {
-  interface IntrinsicElements {
-    "elevenlabs-convai": React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement> & {
-      "agent-id"?: string;
-    };
+import type * as React from "react";
+
+declare module "react" {
+  namespace JSX {
+    interface IntrinsicElements {
+      "elevenlabs-convai": React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement> & {
+        "agent-id"?: string;
+      };
+    }
   }
 }
